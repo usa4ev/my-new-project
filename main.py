2e4f0724f740ace4f06e9558aa5adc24dc8f6e47
pochaka <groznykh.timur@yandex.ru>
2017-09-04 17:21:45 +0300

    Нерабочая версия с красивыми наработками

--- main.py
+++ main.py
@@ -2,39 +2,44 @@ from feedparser import parse
 from sched import scheduler
 from time import time, sleep
 from re import sub, findall
-from datetime import datetime
+from datetime.datetime import _strptime
 from requests import post
+import logging
+import configparser
 
 
-def check(sc, i):
-    bot_name = ''
-    token = ''
-    state = datetime.strftime(datetime.today(), '%c') + ' Итерация:' + str(i)
+# Запрос ленты новостей
+def get_post():
     feed = parse("http://rus.vrw.ru/feed")
-    new_date = feed['channel'].published
-    new_date = parse_date(new_date)
-    with open('my_file.txt') as f:
-        last_date = f.read()
-        if bool(last_date):
-            last_date = datetime.strptime(last_date, '%c %z')
-            if new_date <= last_date:
-                print(state, '- Нет обновлений...')
-                wait(sc, i)
-                return
-        else:
-            with open('my_file.txt', 'w') as f:
-                f.write(datetime.strftime(new_date, '%c %z'))
-    for item in feed.entries:
-        text = "_" + item.category + "_\n\n" \
-               "["+modifikator(item.title)+"]("+item.link+")" + "\n\n" \
-               "" + modifikator(item.description)
-        post("https://api.telegram.org/bot" + bot_name + ":" + token + '/sendMessage?chat_id=@good_news_everybody'
-                                                                       '&parse_mode=Markdown&text=' + text)
-        sleep(1)
-    print(state, '- Публикация обновлена!\n', 'Дата публикации: ' + datetime.strftime(new_date, '%c'))
-    wait(sc, i)
+    return feed
 
 
+# Проверка даты новости
+def check(last_date, new_date):
+    if parse_date(last_date) >= parse_date(new_date):
+        logging.info(u'Новость старая')
+        return False
+    elif parse_date(last_date) <= parse_date(new_date):
+        logging.info(u'Новость новая')
+        return new_date, True
+    else:
+        logging.critical(u'Какой-то пиздец в проверке. Лучше прилягу.')
+        quit()
+        return -1
+
+
+# Отправка сообщения
+def post_message(item, bot_name, bot_token):
+    req_base = "https://api.telegram.org/bot"
+    req_method = '/sendMessage?'
+    req_params = 'chat_id='
+    req_text = ''
+    request = req_base + bot_name + bot_token + req_method + req_params + req_text
+    response = post(request)
+    return
+
+
+# Очистка текста от html-тэгов и замена ascii-кодов на ascii-символы
 def modifikator(text):
     modified_text = sub(r'(<.?([a-z][a-z0-9]*)\b[^>]*>)|Читать полностью »|Обсудить|\s{2,}', '', text)
     modified_text = modified_text.replace('\n', '\n\n')
@@ -49,12 +54,21 @@ def wait(sc, i):
     s.enter(30 * 60, 1, check, (*[sc, i],))
 
 
+# Превращение строки с датой в дату
 def parse_date(str_date):
-    return datetime.strptime(str_date, '%a, %d %b %Y %H:%M:%S %z')
+    return _strptime(str_date, '%a, %d %b %Y %H:%M:%S %z')
 
 
 if __name__ == '__main__':
-    print("running...")
+    # Запуск логгера
+    logging.basicConfig(format=u'%(levelname)-3s [%(asctime)s]  %(message)s',
+                        filename='good_news.log', level=logging.DEBUG)
+    logging.info(u'Бот запущен')
+    # Чтение конфигурации
+    config = configparser.ConfigParser()
+    config.read('good_news.cfg')
+
+
     s = scheduler(time, sleep)
     s.enter(1, 1, check, (*[s, 0], ))
     s.run()
